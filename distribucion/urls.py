c3d86fb4313d7678c61a34e592a44d6c73c7fde9
Nicolás Sirolli <nmsirolli@gmail.com>
2026-07-25 23:12:06 -0300

    Migración a web39

--- distribucion/urls.py
+++ distribucion/urls.py
@@ -15,10 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import RedirectView
 
 urlpatterns = [
     path('materias/', include('materias.urls')),
-    path('encuestas/', include('encuestas.urls')),
+    path('', include('encuestas.urls')),
     path('dborrador/', include('dborrador.urls')),
     path('admin/usuarios/', include('usuarios.urls')),
     path('admin/', admin.site.urls),
